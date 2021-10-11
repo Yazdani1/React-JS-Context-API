a36ea4b7b50d77926a09537c72e179c810b4f119
Yazdani <yaz4dani@gmail.com>
2021-10-11 19:22:52 +0200

    context api

--- client/src/Movie.js
+++ client/src/Movie.js
@@ -3,7 +3,7 @@ import React from "react";
 const Movie = ({name,price,id}) => {
 
   return (
-    <div>
+    <div className="movie_design">
       <h1>{name}</h1>
       <p>{price}</p>
       <p>{id}</p>
